e5e36e4dd2b6d57eb028c3f0d89ada8694808fd7
Vadim Kasatkin <penguin2804@gmail.com>
2021-08-07 17:04:34 +0400

    create store

---
+++ src/configureStore.ts
@@ -0,0 +1,41 @@
+export type Store<State = any, Action = { type: string }> = {
+  getState(): State;
+  dispatch(action: Action): any;
+  subscribe(cb: () => void): () => void;
+};
+
+export type Reducer<State, Action> = (
+  state: State | undefined,
+  action: Action
+) => State;
+export type ConfigureStore<State, Action> = (
+  reducer: Reducer<State, Action>,
+  initialState?: State | undefined,
+) => Store<State, Action>;
+
+type ArrayCb = (() => void)[];
+
+export function configureStore<State, Action>(reducer: Reducer<State, Action>, initialState?: State) {
+  let state = initialState;
+  let subscribeFunctions: ArrayCb = []
+
+  let store = {
+    getState(): State {
+      return state;
+    },
+
+    dispatch(action: Action): void {
+      state = reducer(state, action);
+      subscribeFunctions.forEach(cb => { cb() })
+    },
+
+    subscribe(cb: () => void): () => void {
+      subscribeFunctions.push(cb)
+      return () => {
+        subscribeFunctions = subscribeFunctions.filter(cbEx => cbEx !== cb)
+      }
+    }
+  }
+
+  return store
+}
